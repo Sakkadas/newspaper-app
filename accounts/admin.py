e0fdc413a7aed332b9db590e31fe2e4520fae5d5
Sakkadas <eildos95@gmail.com>
2021-07-30 00:27:22 +0300

    create custom fields

--- accounts/admin.py
+++ accounts/admin.py
@@ -8,6 +8,13 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
+    list_display = ['email', 'username', 'age', 'is_staff', ]
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
