f0c0b21e94263f804567a3b93b4127947f6f700d
Sakkadas <eildos95@gmail.com>
2021-08-02 17:09:03 +0300

    add `UserPassesTestMixin` to all class-based views

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic.edit import (
     UpdateView, DeleteView, CreateView
 )
@@ -17,16 +17,24 @@ class ArticleDetailView(LoginRequiredMixin, DetailView):
     template_name = 'article_detail.html'
 
 
-class ArticleUpdateView(LoginRequiredMixin, UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = ('title', 'body',)
     template_name = 'article_edit.html'
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleDeleteView(LoginRequiredMixin, DeleteView):
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
 
 class ArticleCreateView(LoginRequiredMixin, CreateView):
