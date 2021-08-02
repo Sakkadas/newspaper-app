5d48766ff23f36c028d7dcc29f2a46042fb492c5
Sakkadas <eildos95@gmail.com>
2021-08-02 15:58:08 +0300

    create `ArticleCreateView` view, url path, template

--- articles/urls.py
+++ articles/urls.py
@@ -4,6 +4,7 @@ from .views import (
     ArticleUpdateView,
     ArticleDetailView,
     ArticleDeleteView,
+    ArticleCreateView,
 )
 
 urlpatterns = [
@@ -13,5 +14,6 @@ urlpatterns = [
          ArticleDetailView.as_view(), name='article_detail'),
     path('<int:pk>/delete/',
          ArticleDeleteView.as_view(), name='article_delete'),
+    path('new/', ArticleCreateView.as_view(), name='article_new'),
     path('', ArticleListView.as_view(), name='article_list'),
 ]
