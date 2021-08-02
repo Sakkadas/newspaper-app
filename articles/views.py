80447bcf9940d16be3e091c22707b3d98ab615d4
Sakkadas <eildos95@gmail.com>
2021-08-02 16:55:51 +0300

    link the user to the created article

--- articles/views.py
+++ articles/views.py
@@ -31,4 +31,8 @@ class ArticleDeleteView(DeleteView):
 class ArticleCreateView(CreateView):
     model = Article
     template_name = 'article_new.html'
-    fields = ('title', 'body', 'author',)
+    fields = ('title', 'body',)
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
